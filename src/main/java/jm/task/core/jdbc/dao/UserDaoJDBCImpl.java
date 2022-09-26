4cf5d2ee7f6d5ed39add61c9baaa74c282bee5bb
Данил Любимов <dangor6431@gmail.com>
2022-09-26 10:38:18 +0300

    Сделал реализацию через jdbc

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,88 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    Connection con = new Util().connectionBD();
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
+        try(Statement statement = con.createStatement() ) {
+            statement.execute("CREATE TABLE IF NOT EXISTS users(" +
+                    "id INT AUTO_INCREMENT," +
+                    "name VARCHAR(10)," +
+                    "lastName VARCHAR(20)," +
+                    "age INT NOT NULL, primary key (id))");
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void dropUsersTable() {
+        try (Statement statement = con.createStatement()) {
+            statement.execute("DROP TABLE IF EXISTS users");
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        try (PreparedStatement preparedStatement = con.prepareStatement("INSERT INTO users(name,lastName,age) VALUES (?,?,?)")) {
 
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeUserById(long id) {
+        try(PreparedStatement ps = con.prepareStatement("DELETE FROM users WHERE id = ?")) {
+            ps.setLong(1, id);
+            ps.executeUpdate();
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> list = new ArrayList<>();
+        try(Statement st = con.createStatement()) {
+            ResultSet resultSet = st.executeQuery("SELECT * FROM users");
+
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong(1));
+                user.setName(resultSet.getString(2));
+                user.setLastName(resultSet.getString(3));
+                user.setAge(resultSet.getByte(4));
+                list.add(user);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
     }
-
     public void cleanUsersTable() {
+        try(Statement st = con.createStatement()) {
+            st.execute("TRUNCATE TABLE users");
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/pp_1_1_3?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true" +
+            "&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String ROOT = "root";
+    private static final String PASSWORD = "admin";
+
+    public Connection connectionBD() {
+        try  {
+            Connection connection = DriverManager.getConnection(URL, ROOT, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("Подключение к БД выполнено");
+            }
+            return connection;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
